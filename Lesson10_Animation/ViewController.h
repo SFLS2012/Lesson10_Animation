f8644ed60de78e45257178fa8d75dec34e3312c0
Harebert <harebert2008@163.com>
2012-12-19 11:48:54 +0800

    finish

--- Lesson10_Animation/ViewController.h
+++ Lesson10_Animation/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController{
+    UIImageView *newImageView;
+}
+@property(retain,nonatomic)UIImageView *newlyImageView;
 @end
--- Lesson10_Animation/ViewController.m
+++ Lesson10_Animation/ViewController.m
@@ -13,9 +13,25 @@
 @end
 
 @implementation ViewController
-
+@synthesize newlyImageView;
 - (void)viewDidLoad
 {
+    newlyImageView=[[UIImageView alloc]initWithFrame:CGRectMake(135, 100, 50, 50)];
+    newlyImageView.backgroundColor=[UIColor redColor];
+    [self.view addSubview:newlyImageView];
+    
+    UIButton *leftButton=[UIButton buttonWithType:UIButtonTypeRoundedRect];
+    leftButton.frame=CGRectMake(120, 200, 80, 40);
+    [leftButton setTitle:@"左移" forState:UIControlStateNormal];
+    [leftButton addTarget:self action:@selector(moveLeft) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:leftButton];
+    
+    UIButton *rightButton=[UIButton buttonWithType:UIButtonTypeRoundedRect];
+    rightButton.frame=CGRectMake(120, 260, 80, 40);
+    [rightButton setTitle:@"右移" forState:UIControlStateNormal];
+    [rightButton addTarget:self action:@selector(moveRight) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:rightButton];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -26,4 +42,14 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)moveLeft
+{
+    NSLog(@"left");
+    newlyImageView.frame=CGRectMake(newlyImageView.frame.origin.x-1, newlyImageView.frame.origin.y, newlyImageView.frame.size.width, newlyImageView.frame.size.height);
+}
+-(void)moveRight
+{
+    NSLog(@"right");
+    newlyImageView.frame=CGRectMake(newlyImageView.frame.origin.x+1, newlyImageView.frame.origin.y, newlyImageView.frame.size.width, newlyImageView.frame.size.height);
+}
 @end
